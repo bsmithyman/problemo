583779a39a68e73f7f0e0f16f28406f799be0cb5
Brendan Smithyman <brendan@bitsmithy.net>
2016-04-29 16:18:19 -0400

    Try to wrap three packages, gracefully

--- problemo/solverwrap.py
+++ problemo/solverwrap.py
@@ -10,40 +10,58 @@ import numpy as np
 import scipy.sparse
 import scipy.sparse.linalg
 
+DefaultSolver = None
+
+active = []
+solvers = {}
+
 try:
-    from pymatsolver import MumpsSolver
-    DefaultSolver = MumpsSolver
+    from pymatsolver import MumpsSolver as _MumpsSolver
 except (ImportError, SyntaxError):
-    DefaultSolver = scipy.sparse.linalg.splu
+    _MumpsSolver = None
+else:
+    active.append('mumps')
+
+try:
+    from pyMKL import pardisoSolver as _MKLPardisoSolver
+except ImportError:
+    _MKLPardisoSolver = None
+else:
+    active.append('mklpardiso')
+
+try:
+    from scipy.sparse.linalg import splu as _SuperLUSolver
+except ImportError:
+    _SuperLUSolver = None
+else:
+    active.append('splu')
+
+if not solvers:
+    raise ImportError('Problemo: could not load backend')
 
 
-class DirectSolver(object):
+class BaseSolver(object):
     '''
     Wrapper around a direct sparse system solver.
     '''
 
-    def __init__(self, Solver=None):
+    Solver = None
+
+    def __init__(self, A=None):
         '''
         Initialize solver
 
         Args:
-            Solver (class): The class to instantiate as the low-level solver
+            A (sparse matrix): The matrix to solve
         '''
 
-        self._Solver = Solver
-
-    @property
-    def Solver(self):
-        'Returns the solver class of choice'
-        if getattr(self, '_Solver', None) is None:
-            self._Solver = DefaultSolver
-        return self._Solver
+        self.A = A
 
     @property
     def Ainv(self):
         'Returns a Solver instance'
 
-        if getattr(self, '_Ainv', None) is None:
+        if not hasattr(self, '_Ainv'):
             self._Ainv = self.Solver(self.A)
         return self._Ainv
 
@@ -56,6 +74,8 @@ class DirectSolver(object):
         return self._A
     @A.setter
     def A(self, A):
+        if A is None:
+            return
         if isinstance(A, scipy.sparse.spmatrix):
             self._A = A
         else:
@@ -76,13 +96,6 @@ class DirectSolver(object):
             np.ndarray: Wavefields
         '''
 
-        if hasattr(self.Ainv, '__mul__'):
-            action = lambda b: self.Ainv * b
-        elif hasattr(self.Ainv, 'solve'):
-            action = lambda b: self.Ainv.solve(b)
-        else:
-            raise Exception('Can\'t interpret how to use solver class %s'%(self.Ainv.__class__.__name__,))
-
         if isinstance(rhs, scipy.sparse.spmatrix):
             def qIter(qs):
                 for j in range(qs.shape[1]):
@@ -92,12 +105,71 @@ class DirectSolver(object):
         else:
             def qIter(qs):
                 for j in range(qs.shape[1]):
-                    qi = qs[:,j]
+                    qi = qs[:, j]
                     yield qi
                 return
 
         result = np.empty(rhs.shape, dtype=np.complex128)
         for i, q in enumerate(qIter(rhs)):
-            result[:,i] = action(q)
+            result[:, i] = self._solve(q)
 
         return result
+
+    def _solve(self, rhs):
+        raise NotImplementedError
+
+
+class MumpsSolver(BaseSolver):
+    '''
+    Wrapper around MUMPS, via pymatsolver
+    '''
+
+    Solver = _MumpsSolver
+
+    def _solve(self, rhs):
+        return self.Ainv * rhs
+
+solvers['mumps'] = MumpsSolver
+
+
+class MKLPardisoSolver(BaseSolver):
+    '''
+    Wrapper around MKL PARDISO, via pyMKL
+    '''
+
+    Solver = _MKLPardisoSolver
+
+    @property
+    def Ainv(self):
+        'Returns a Solver instance'
+
+        if getattr(self, '_Ainv', None) is None:
+            self._Ainv = self.Solver(self.A, 13)
+            self._Ainv.run_pardiso(12)
+        return self._Ainv
+
+    def _solve(self, rhs):
+        return self.Ainv.run_pardiso(33, rhs)
+
+    def __del__(self):
+        if hasattr(self, '_Ainv'):
+            self._Ainv.run_pardiso(-1)
+            del self._Ainv
+
+solvers['mklpardiso'] = MKLPardisoSolver
+
+
+class SuperLUSolver(BaseSolver):
+    '''
+    Wrapper around SuperLU, via scipy.sparse.linalg.splu
+    '''
+
+    Solver = _SuperLUSolver
+
+    def _solve(self, rhs):
+        return self.Ainv.solve(rhs)
+
+solvers['splu'] = SuperLUSolver
+
+
+BestSolver = solvers[active[0]]
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open('README.md') as fp:
 
 setup(
     name = 'problemo',
-    version = '0.1.4',
+    version = '0.2.0',
     packages = find_packages(),
     install_requires = ['numpy>=1.7',
                         'scipy>=0.13',
